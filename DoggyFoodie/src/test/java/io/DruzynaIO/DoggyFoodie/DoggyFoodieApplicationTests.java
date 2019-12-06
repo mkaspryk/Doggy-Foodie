bfc8320027711dc316e79bd355a7fd96052fac3a
mkaspryk <mkaspryk09@outlook.com>
2019-12-06 15:00:38 +0100

    Preparing Framework and IDE

---
+++ DoggyFoodie/src/test/java/io/DruzynaIO/DoggyFoodie/DoggyFoodieApplicationTests.java
@@ -0,0 +1,13 @@
+package io.DruzynaIO.DoggyFoodie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggyFoodieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
