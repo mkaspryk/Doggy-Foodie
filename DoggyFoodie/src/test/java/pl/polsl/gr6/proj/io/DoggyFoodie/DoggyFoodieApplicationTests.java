faf44299a16ddb440069d088be36ae64c81c6b4b
cptrko <32636603+cptrko@users.noreply.github.com>
2020-01-25 18:22:33 +0100

    Setting up project's environment

---
+++ DoggyFoodie/src/test/java/pl/polsl/gr6/proj/io/DoggyFoodie/DoggyFoodieApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.gr6.proj.io.DoggyFoodie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggyFoodieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
